ef5854ebe35a173405d3cb2a6ddf32b207dc1e41
mbuergi <m.buergi@linguala.com>
2024-09-27 10:25:54 +0200

    feat(core): ✨ Add Echo integration and services logic

--- cmd/main.go
+++ cmd/main.go
@@ -1,13 +1,32 @@
 package main
 
 import (
+	"database/sql"
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
 )
 
 func main() {
 	e := echo.New()
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	connStr := "user=postgres dbname=users sslmode=disable"
+
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatalf("Error opening database: %q", err)
+	}
+
+	defer db.Close()
+
+	if len(os.Args) > 1 && os.Args[1] == "seed" {
+	}
 
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
