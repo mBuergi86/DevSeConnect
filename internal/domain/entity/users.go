f2c1966b61af7a3f754b241760d6b774a9ec4a86
mBuergi86 <markus.buergi1986@gmail.com>
2024-10-02 10:56:28 +0200

    ✨ Update User entity customised

--- internal/domain/entity/users.go
+++ internal/domain/entity/users.go
@@ -1,7 +1,6 @@
 package entity
 
 import (
-	"errors"
 	"time"
 
 	"github.com/google/uuid"
@@ -9,30 +8,21 @@ import (
 )
 
 type User struct {
-	UserID   uuid.UUID `db:"user_id" json:"user_id"`
-	Username string    `db:"username" json:"username"`
-	Email    string    `db:"email" json:"email"`
-	Password string    `db:"password_hash" json:"-"`
-
-	FirstName      string    `db:"first_name" json:"first_name"`
-	LastName       string    `db:"last_name" json:"last_name"`
-	Bio            string    `db:"bio" json:"bio"`
-	ProfilePicture string    `db:"profile_picture" json:"profile_picture"`
-	IsActive       bool      `db:"is_active" json:"is_active"`
-	CreatedAt      time.Time `db:"created_at" json:"created_at"`
-	UpdatedAt      time.Time `db:"updated_at" json:"updated_at"`
-}
-
-func (u *User) GenerateFullName() string {
-	return u.FirstName + " " + u.LastName
+	UserID         uuid.UUID `gorm:"primaryKey;type:uuid;default:uuid_generate_v4()" json:"user_id"`
+	Username       string    `gorm:"uniqueIndex;not null" json:"username"`
+	Email          string    `gorm:"uniqueIndex;not null" json:"email"`
+	PasswordHash   string    `gorm:"column:password_hash;not null" json:"-"` // Geändert zu PasswordHash
+	FirstName      string    `json:"first_name"`
+	LastName       string    `json:"last_name"`
+	Bio            string    `json:"bio"`
+	ProfilePicture string    `json:"profile_picture"`
+	IsActive       bool      `gorm:"default:true" json:"is_active"`
+	CreatedAt      time.Time `gorm:"default:CURRENT_TIMESTAMP" json:"created_at"`
+	UpdatedAt      time.Time `gorm:"default:CURRENT_TIMESTAMP" json:"updated_at"`
 }
 
 func NewUsers(username, email, password, firstName, lastName, bio, profilePicture string) (*User, error) {
-	if username == "" || email == "" || password == "" {
-		return nil, errors.New("username, email, and password are required")
-	}
-
-	passwordHash, err := security.Hash(password)
+	hashedPasswort, err := security.Hash(password)
 	if err != nil {
 		return nil, err
 	}
@@ -41,7 +31,7 @@ func NewUsers(username, email, password, firstName, lastName, bio, profilePictur
 		UserID:         uuid.New(),
 		Username:       username,
 		Email:          email,
-		Password:       string(passwordHash),
+		PasswordHash:   string(hashedPasswort),
 		FirstName:      firstName,
 		LastName:       lastName,
 		Bio:            bio,
@@ -50,4 +40,17 @@ func NewUsers(username, email, password, firstName, lastName, bio, profilePictur
 		CreatedAt:      time.Now(),
 		UpdatedAt:      time.Now(),
 	}, nil
+}
+
+func (u *User) SetPassword(password string) error {
+	hashedPassword, err := security.Hash(password)
+	if err != nil {
+		return err
+	}
+	u.PasswordHash = string(hashedPassword)
+	return nil
+}
+
+func (u *User) CheckPassword(password string) bool {
+	return security.CheckPasswordHash(u.PasswordHash, password)
 }
