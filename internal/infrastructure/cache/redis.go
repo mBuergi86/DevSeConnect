ba730864e5b72ea77c2b4bc1a0a488b7f1fb88a4
mBuergi86 <markus.buergi1986@gmail.com>
2024-10-06 13:53:29 +0200

    refactor(db):♻️ Update PostgreSQL and Redis Cache

--- internal/infrastructure/cache/redis.go
+++ internal/infrastructure/cache/redis.go
@@ -3,15 +3,18 @@ package cache
 import (
 	"context"
 	"os"
+	"strings"
 
 	"github.com/redis/go-redis/v9"
 )
 
 func InitRedis() (*redis.Client, error) {
+	redisURL := os.Getenv("REDIS_URL")
+	redisAddr := strings.TrimPrefix(redisURL, "redis://")
 	client := redis.NewClient(&redis.Options{
-		Addr:     os.Getenv("REDIS_ADDR"),
-		Password: os.Getenv("REDIS_PASSWORD"),
-		DB:       0,
+		Addr: redisAddr,
+		// Password: os.Getenv("REDIS_PASSWORD"),
+		DB: 0,
 	})
 
 	_, err := client.Ping(context.Background()).Result()
--- internal/infrastructure/database/postgres.go
+++ internal/infrastructure/database/postgres.go
@@ -9,12 +9,12 @@ import (
 )
 
 func InitPostgres() (*gorm.DB, error) {
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
-		os.Getenv("DB_HOST"),
+	dsn := fmt.Sprintf("postgresql://%s:%s@%s:%s/%s?sslmode=disable",
 		os.Getenv("DB_USER"),
 		os.Getenv("DB_PASSWORD"),
-		os.Getenv("DB_NAME"),
+		os.Getenv("DB_HOST"),
 		os.Getenv("DB_PORT"),
+		os.Getenv("DB_NAME"),
 	)
 
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
