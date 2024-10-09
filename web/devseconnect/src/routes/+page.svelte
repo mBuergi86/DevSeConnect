6ce9dd01bac17bc9eb6b30da7f4744c146906b39
mBuergi86 <markus.buergi1986@gmail.com>
2024-10-09 18:44:38 +0200

    feat(page): ✨ Update or add features to +page.svelte

--- web/devseconnect/src/routes/+page.svelte
+++ web/devseconnect/src/routes/+page.svelte
@@ -1,2 +1,13 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+
+	let { children, prefix } = $props();
+	prefix = prefix as Snippet;
+</script>
+
+<div>
+	<h1>Welcome to SvelteKit</h1>
+	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+	{@render children?.()}
+	{@render prefix?.()}
+</div>
