25c08df8d1b2477b19de44e6753400fd8d1f1c6e
mBuergi86 <markus.buergi1986@gmail.com>
2024-10-17 18:21:25 +0200

    feat(layout): ✨ added server-side logic for dashboard layout

--- web/devseconnect/src/routes/dashboard/+layout.server.ts
+++ web/devseconnect/src/routes/dashboard/+layout.server.ts
@@ -3,10 +3,19 @@ import type { LayoutServerLoad } from './$types';
 
 export const load: LayoutServerLoad = async ({ cookies }) => {
 	const isLoggedIn = cookies.get('token') ? true : false;
+
 	if (!isLoggedIn) {
 		redirect(302, '/home');
 	}
-	return {
-		isLoggedIn
-	};
+
+	try {
+		return {
+			isLoggedIn
+		};
+	} catch (error) {
+		console.error(error);
+		return {
+			error: 'Failed to load user data'
+		};
+	}
 };
