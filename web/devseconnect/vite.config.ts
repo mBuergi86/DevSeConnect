23b7bb277f422fa728d8d13963cc68bc37b5b0bb
mBuergi86 <markus.buergi1986@gmail.com>
2024-10-17 14:06:14 +0200

    chore(config):🛠️ update svelte and vite configuration

--- web/devseconnect/vite.config.ts
+++ web/devseconnect/vite.config.ts
@@ -1,5 +1,20 @@
 import { defineConfig } from 'vitest/config';
 import { sveltekit } from '@sveltejs/kit/vite';
+//import { Server } from 'socket.io';
+//import type { ViteDevServer } from 'vite';
+
+/*const webSocketServer = {
+	name: 'webSocketServer',
+	configureeServer(server: ViteDevServer) {
+		if (!server.httpServer) return;
+
+		const io = new Server(server.httpServer);
+
+		io.on('connection', (socket) => {
+			socket.emit('eventFromServer', 'Hello from server');
+		});
+	}
+};*/
 
 export default defineConfig({
 	plugins: [sveltekit()],
