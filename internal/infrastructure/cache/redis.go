5c816016829aa3d51ae841d1aafd0cce3a5a87a3
mBuergi86 <markus.buergi1986@gmail.com>
2024-10-30 18:46:56 +0100

    chore(redis-cache): 🔧 Refactor Redis cache settings for performance

--- internal/infrastructure/cache/redis.go
+++ internal/infrastructure/cache/redis.go
@@ -12,9 +12,9 @@ func InitRedis() (*redis.Client, error) {
 	redisURL := os.Getenv("REDIS_URL")
 	redisAddr := strings.TrimPrefix(redisURL, "redis://")
 	client := redis.NewClient(&redis.Options{
-		Addr: redisAddr,
-		// Password: os.Getenv("REDIS_PASSWORD"),
-		DB: 0,
+		Addr:     redisAddr,
+		Password: os.Getenv("REDIS_PASSWORD"),
+		DB:       0,
 	})
 
 	_, err := client.Ping(context.Background()).Result()
