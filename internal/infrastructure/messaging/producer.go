e529a572ace687f2478f9f1d53105df6768c99ac
mBuergi86 <markus.buergi1986@gmail.com>
2024-10-30 18:47:03 +0100

    feat(producer): ✨ Add enhanced logging to producer messages

--- internal/infrastructure/messaging/producer.go
+++ internal/infrastructure/messaging/producer.go
@@ -3,34 +3,66 @@ package messaging
 import (
 	"context"
 	"encoding/json"
+	"fmt"
+	"os"
 	"time"
 
+	"github.com/mBuergi86/devseconnect/internal/domain/models"
 	"github.com/rabbitmq/amqp091-go"
+	"github.com/rs/zerolog"
 )
 
 type Producer struct {
 	channel *amqp091.Channel
+	logger  zerolog.Logger
 }
 
 func NewProducer(conn *amqp091.Connection) (*Producer, error) {
 	ch, err := conn.Channel()
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("failed to open a channel: %w", err)
 	}
-	return &Producer{channel: ch}, nil
+
+	err = ch.ExchangeDeclare(
+		"user_events",
+		"direct",
+		true,
+		false,
+		false,
+		false,
+		nil,
+	)
+	if err != nil {
+		return nil, fmt.Errorf("failed to declare exchange: %w", err)
+	}
+
+	return &Producer{channel: ch, logger: zerolog.New(os.Stdout).With().Timestamp().Str("component", "producer").Logger()}, nil
 }
 
-func (p *Producer) PublishMessage(exchange, routingKey string, message interface{}) error {
+func (p *Producer) PublishMessage(exchange, routingKey string, message models.EventMessage) error {
 	body, err := json.Marshal(message)
 	if err != nil {
-		return err
+		p.logger.Error().Err(err).Msg("Failed to marshal complete message")
+		return fmt.Errorf("failed to marshal complete message: %w", err)
 	}
 
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
-	return p.channel.PublishWithContext(ctx, exchange, routingKey, false, false, amqp091.Publishing{
-		ContentType: "application/json",
-		Body:        body,
-	})
+	err = p.channel.PublishWithContext(
+		ctx,
+		exchange,
+		routingKey,
+		false,
+		false,
+		amqp091.Publishing{
+			ContentType: "application/json",
+			Body:        body,
+		})
+	if err != nil {
+		p.logger.Error().Err(err).Msg("Failed to publish message")
+		return fmt.Errorf("failed to publish message: %w", err)
+	}
+
+	return nil
 }
